8ec871bedb7f213889cc628384b3a43eda035a20
naseem wali khan <naseemwalikhan776@gmail.com>
2024-10-01 12:26:19 -0700

    addedsomework

---
+++ index.js
@@ -0,0 +1,86 @@
+// STRINGS METHODS  QUIZ QUESTIONS
+
+
+// 1. Write a program that takes two user inputs for first and
+// last name using prompt and merge them in a new variable
+// titled fullName. Greet the user using his full name.
+
+// var userinput1=prompt('Enter you first anme');
+// userinput1=userinput1.toLowerCase();
+// var userinput2=prompt('Enter your lastname');
+// userinput2=userinput2.toLowerCase()
+
+// var Fullname=userinput1 + " " +  userinput2;
+
+//     alert('your full name is ' +  " " + Fullname);
+
+   // my method
+
+// var userinput1=prompt('Enter you first anme');
+// userinput1=userinput1.toLowerCase();
+// var userinput2=prompt('Enter your lastname');
+// userinput2=userinput2.toLowerCase()
+
+// var Fullname=userinput1 + userinput2;
+
+// if (userinput1+userinput2==Fullname) {
+//     alert('your full name is ' + ' ' + Fullname);
+//    }
+  
+
+// Q 2. Write a programme  to take a user input about his favorite
+// mobile phone model. Find and display the length of user
+// input in your browser?
+//var userinput=prompt('enter your favourite mobile phone model');
+//  document.write('My favourite phone is : ' +  userinput + '<br>' + ' Length is : '  +   userinput.length);
+
+
+// 3. Write a program to find the index of letter “n” in the word
+// “Pakistani” and display the result in your browser .
+    
+
+// var string='pakistan';
+// var d=string.indexOf('n');
+// document.write('String : '+  string + '<br> ' + 'index of "n" : '  + d);
+
+
+
+// 4. Write a program to find the last index of letter “l” in the
+// word “Hello World” and display the result in your browser.
+
+
+// var e='hello world';
+// f=e.lastIndexOf('l');
+// document.write('string : ' + e + '<br>' +  ' last  index of l : ' + f);
+
+
+// 5. Write a program to find the character at 3rd index in the
+// word “Pakistani” and display the result in your browser.
+
+// var s='pakistani'; 
+// let r=s.charAt(3);
+// document.write('string : pakistan' + '<br>' + 'character at index 3 : ' + r)
+
+
+// Q6 already done with both methods in q1
+
+
+// 7. Write a program to replace the “Hyder” to “Islam” in the
+// word “Hyderabad” and display the result in your browser.
+
+// var z='Hyderabad';
+// for (let index = 0; index < z.length; index++) {
+//     if (z.slice(index,index+5)==='Hyder') {
+//       var h=  "Islam"+ z.slice(index+5)
+//       document.write('city : ' + z + ' <br> ' + 'After replacement: ' + h)
+      
+//     }
+    
+// }
+
+
+// by using replace method
+
+
+
+
